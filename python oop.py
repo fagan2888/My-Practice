1120ef099cef00500688b8c0ca454c9a00529636
Lingyi Xu <35391238+lingyixu@users.noreply.github.com>
2019-05-19 16:47:17 -0400

    Add files via upload

---
+++ python oop.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 11 15:09:52 2018
+
+@author: Olivia XU
+"""
+
+class Dog:
+    
+    # Class Attribute
+    species='mammal'
+    
+    # Initializer / Instance Attributes / Constructor
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+    # instance method
+    def description(self):
+        return '{} is {} years old'.format(self.name, self.age)
+    
+    # instance method
+    def sound(self, sound):
+        return '{} says {}'.format(self.name, sound)
+    
+    def __add__(self,other):
+        return Dog(self.name, self.age+other.age)
+
+
+# Child class (inherits from Dog class)
+class RussellTerrier(Dog):
+    def run(self, speed):
+        return '{} runs {}'.format(self.name, speed)
+
+# Child classes inherit attributes and behaviors from the parent class
+jim = RussellTerrier("Jim", 12)
+print(jim.description())
+
+# Child classes have specific attributes and behaviors as well
+print(jim.run("slowly"))
+
+# Is jim an instance of Dog()?
+print(isinstance(jim, Dog))
+
+# Is julie an instance of Dog()?
+julie = Dog("Julie", 100)
+print(isinstance(julie, Dog))
+
+# Is julie and instance of jim?
+print(isinstance(julie, jim))
+
+# Child class could override attributes and behavior of parent class
+class SomeOtherBreed(Dog):
+    species = 'reptile'
+
+beans = SomeOtherBreed('unknown',0)
+print(beans.species)
+
+
+a = Dog('A',1)
+b = Dog('B',2)
+c = Dog('C',3)
+print(a.description())
+print(b.sound('wang wang'))
+
+def get_biggest_age(*args):
+    return max(args)
+print("The oldest dog is {} years old.".format(get_biggest_age(a.age, b.age, c.age)))
+
+
+
+import numpy as np
+a1=np.array([[1,2],[3,4]])
+a2=np.array([[2,4],[6,8]])
+m1=np.matrix('1 2;3 4')
+m2=np.matrix('2 4;6 8')
+am1=np.array(m1)
+am2=np.array(m2)
+arr1=np.array([1,2,3,4,5,6])
+arr2=np.array([56,79,31,50,48,26])
+np.cov(arr1,arr2)
+np.corrcoef(arr1,arr2)
+
+
+import pandas as pd
+df1=pd.DataFrame(np.random.random(size=(5,1)),columns=['classA'])
+df2=pd.DataFrame(np.random.random(size=(5,1)),columns=['classB'])
+df1.join(df2)
+pd.concat([df1,df2],axis=1)
+
+
+
+import math
+from scipy.stats import norm
+def callPx(S_0,k,r,sigma,tau):
+    sigmaRtT=(sigma*math.sqrt(tau))
+    rSigTerm=(r+sigma**2/2.0)*tau
+    d1=(math.log(S_0/k)+rSigTerm)/sigmaRtT
+    d2=d1-sigmaRtT
+    term1=S_0*norm.cdf(d1)
+    term2=k*math.exp(-r*tau)*norm.cdf(d2)
+    return term1+term2
+
+
+
+import pandas as pd
+df1 = pd.DataFrame(['K0', 'K1', 'K2', 'K3', 'K4', 'K5'], columns=['etf1'], index=['20', '21', '22', '23', '24', '25'])
+df2 = pd.DataFrame(['K0', 'K1', 'K2'], columns=['etf2'], index=['20', '21', '22'])
+
+
+
+
+
+class BasePosition:
+    def __init__ (self , shrs , long ):
+        self . shares = shrs
+        self . isLong = long
+        print (" calling BasePosition class constructor ")
+
+    def __del__ ( self ):
+        print (" calling BasePosition class destructor ")
+
+    def printPos ( self ):
+        print (" calling printPos from Base ")
+        print ( self . shares )
+        print ( self . isLong )
+
+class ChildPosition (BasePosition):
+    def __init__ (self , shrs , long , childShrs ):
+            self . childShares = childShrs
+            BasePosition . __init__ (self , shrs , long )
+            print (" calling ChildPosition class constructor ")
+
+    def __del__ ( self ):
+        print (" calling ChildPosition class destructor ")
+
+    def printPos ( self ):
+        print (" calling printPos from Child ")
+        print ( self . shares )
+        print ( self . isLong )
+        print ( self . childShares )
+
+basePos1 = BasePosition (100 , 1)
+basePos2 = BasePosition (75 , 0)
+
+childPos1 = ChildPosition (25 , 0, 5)
+childPos2 = ChildPosition (150 , 1, 0)
+
+basePos3 = basePos1 + basePos2
+
